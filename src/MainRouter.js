2cdc2f4f2a41791747b3440cc2c91857be53186d
kylexrose@gmail.com <kylexrose@gmail.com>
2021-07-16 22:24:14 -0400

    initial commit

---
+++ src/MainRouter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class MainRouter extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default MainRouter
