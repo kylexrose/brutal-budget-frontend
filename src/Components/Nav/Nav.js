bc725d8b9fe4409b51b703591a03011229c7b0fd
kylexrose@gmail.com <kylexrose@gmail.com>
2021-07-19 20:49:12 -0400

    more functionality added to navbar. TODO: debug login edge-case

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react'
 import './Nav.css';
 import logo from "../../local/budgetLogo.png"
+import { NavLink } from 'react-router-dom';
 
 export class Nav extends Component {
     render() {
         return (
             <div className="navbar">
-            <img src={logo} alt="logo"/>
-            <p>Brutal Budget.</p>
-        </div>
+                <div className="menuIcon">
+                    <img src="#"/>
+                </div>
+                <div className="logoContainer">
+                    <img src={logo} alt="logo"/>
+                    <p>Brutal Budget.</p>
+                </div>
+                <div className="right-side-nav">
+                    <ul>
+                        <li>
+                        {this.props.user ? (
+                            <NavLink activeClassName="selected" to="/profile">
+                            Profile
+                            </NavLink>
+                        ) : (
+                            <NavLink activeClassName="selected" to="/sign-up">
+                            Sign up
+                            </NavLink>
+                        )}
+                        </li>
+                        <li>
+                        {this.props.user ? (
+                            <NavLink
+                            activeStyle={{ borderBottom: "1px solid white" }}
+                            to="/home"
+                            onClick={this.props.handleUserLogout}
+                            >
+                            Logout
+                            </NavLink>
+                        ) : (
+                            <NavLink
+                            activeStyle={{ borderBottom: "1px solid white" }}
+                            to="/login"
+                            >
+                            Login
+                            </NavLink>
+                        )}
+                        </li>
+                    </ul>
+                </div>
+            </div>
         )
     }
 }
