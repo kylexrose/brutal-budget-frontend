2a5bd8539f91e1b739cb6667a512e777f9293dfd
kylexrose@gmail.com <kylexrose@gmail.com>
2021-08-18 12:50:31 -0400

    income now runs on hooks

--- src/Components/EnterIncome/Income.js
+++ src/Components/EnterIncome/Income.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Income.css';
 import Axios from '../utils/Axios';
 import {toast} from 'react-toastify';
 import { Link } from 'react-router-dom';
 
-export class Income extends Component {
-    state = {
-        date: "",
-        description: "",
-        amount: "",
-        amountError: "", 
-    }
+function Income() {
+    const [date, setDate] = useState("");
+    const [description, setDescription] = useState("");
+    const [amount, setAmount] = useState("");
+    const [amountError, setAmountError] = useState("");
 
-    componentDidMount() {
-        this.setState({
-            date: JSON.stringify(new Date(Date.now())).slice(1, 11)
-        })
-    }
+    useEffect(() => {
+        setDate(JSON.stringify(new Date(Date.now())).slice(1, 11))
+    }, [])
     
-    handleOnChange = (event) =>{
-        this.setState({
-            [event.target.id]: event.target.value
-        })
+    function handleOnChange(event) {
+        if(event.target.id === "date"){
+            setDate(event.target.value)
+        }else if(event.target.id === "amount"){
+            setAmount(event.target.value)
+        }else if(event.target.id === "description"){
+            setDescription(event.target.value)
+        }
     }
 
-    handleOnSubmit = async (event) =>{
+    async function handleOnSubmit(event) {
         event.preventDefault();
-        if(!this.state.amountError){
+        if(!amountError){
             try{
-            const convDate = this.state.date.split("-");
+            const convDate = date.split("-");
             const dateObj = {
                 year: +convDate[0],
                 month: +convDate[1],
@@ -36,16 +36,14 @@ export class Income extends Component {
             }
             const newTransaction = {
                 date: dateObj,
-                description: this.state.description,
-                amount: this.state.amount,
+                description: description,
+                amount: amount,
                 type: "Income"
             }
             await Axios.post('api/transactions/create-new-transaction', newTransaction)
-            this.setState({
-                date: JSON.stringify(new Date(Date.now())).slice(1, 11),
-                description: "",
-                amount: ""
-            })
+            setDate(JSON.stringify(new Date(Date.now())).slice(1, 11));
+            setDescription("");
+            setAmount("");
             toast.success(`Income Added`)
             }catch(e){
                 console.log(e)
@@ -53,44 +51,43 @@ export class Income extends Component {
         }
         
     }
-    
-    render() {
-        return (
-            <div className="main">
-                <Link className="addButton back" to="/overview">Back</Link>
-                <div className= "moneySign">
-                    <h3>Income</h3>
-                </div>
-                <div className="input">
-                    <form onSubmit={this.handleOnSubmit}>
-                        <label htmlFor="date">Date</label>
-                        <input 
-                        type= "date"
-                        id= "date"
-                        name= "date" 
-                        value={`${this.state.date}`}
-                        onChange={this.handleOnChange}
-                        />
-                        <label htmlFor="description">Description</label>
-                        <input 
-                        type= "text"
-                        id= "description"
-                        name= "description" 
-                        value={this.state.description}
-                        onChange={this.handleOnChange}/>
-                        <label htmlFor="amount">Amount</label>
-                        <input 
-                        type= "text"
-                        id= "amount"
-                        name= "amount" 
-                        value={this.state.amount}
-                        onChange={this.handleOnChange}/>
-                        <button className="submit">Enter Income</button>
-                    </form>
-                </div>
+    return (
+        <div className="main">
+            <Link className="addButton back" to="/overview">Back</Link>
+            <div className= "moneySign">
+                <h3>Income</h3>
             </div>
-        )
-    }
+            <div className="input">
+                <form onSubmit={this.handleOnSubmit}>
+                    <label htmlFor="date">Date</label>
+                    <input 
+                    type= "date"
+                    id= "date"
+                    name= "date" 
+                    value={`${this.state.date}`}
+                    onChange={this.handleOnChange}
+                    />
+                    <label htmlFor="description">Description</label>
+                    <input 
+                    type= "text"
+                    id= "description"
+                    name= "description" 
+                    value={this.state.description}
+                    onChange={this.handleOnChange}/>
+                    <label htmlFor="amount">Amount</label>
+                    <input 
+                    type= "text"
+                    id= "amount"
+                    name= "amount" 
+                    value={this.state.amount}
+                    onChange={this.handleOnChange}/>
+                    <button className="submit">Enter Income</button>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default Income
+
+
