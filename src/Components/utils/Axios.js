54ffeb04ed72b762244a5a6e8b5b7aecf298e63e
kylexrose@gmail.com <kylexrose@gmail.com>
2021-10-07 22:58:28 -0400

    changes made for deployment

--- src/Components/utils/Axios.js
+++ src/Components/utils/Axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const Axios = axios.create({
     baseURL: process.env.NODE_ENV === "development" 
         ? "http://localhost:8080" 
-        : "DEPLOY CLOUD ADDRESS",
+        : "brutalbudget.kylexrose.com",
     timeout: 50000,
     })
 
